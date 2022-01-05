4f48a80555123318c1552c6eaf97a1281bafdd03
hysong <hachu.developer@gmail.com>
2022-01-05 12:01:13 +0900

    팝업 layer 작업 중

--- src/images/Image.tsx
+++ src/images/Image.tsx
@@ -61,43 +61,106 @@ const FirstImg = styled.img`
     margin: 10px 0 0 0;
 `;
 
+const PopUpImg = styled.div`
+    border-radius: 5px;
+    // width: 100%;
+    // height: 100%;
+    // object-fit: cover;
+    // margin: 10px 0 0 0;
+    padding: 10px 0 50px;
+    background-color: #FDFAF6;
+    z-index: 2;
+    align-items: center;
+    position: fixed;
+    top: 120px;
+    // left: 5px;
+    justify-content: center;
+    flex-direction: column;
+    display: ${(props: {layerActive: boolean}) => props.layerActive ? 'flex' : 'none'};
+`
+
+const CloseBtn = styled.span`
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 30px;
+    font-weight: bold;
+    color: #A1CAE2;
+    cursor: pointer;
+    margin: 0 0 10px 0;
+    &:hover {
+      text-shadow: 0 0 10px #A1CAE2;
+    }
+`
+
+const LayerImg = styled.img`
+    border-radius: 5px;
+    width: 90%;
+    height:80%;
+    object-fit: cover;
+    // margin: 10px 0 0 0;
+`;
+
 const [imageActive, setImageActive] = useState(false);
+const [clickedIndex, setClickedIndex] = useState(0);
 
   return (
-    <><ImageDefaultContainer active={imageActive}>
-      <DefaultImg src="/images/couple/blue-shy.jpeg" />
-      <DefaultImg src="/images/couple/couple-shoes.jpeg" />
-      <DefaultImg src="/images/couple/next-line.jpeg" />
-      <DefaultImg src="/images/couple/kuho.jpeg" />
-      <DefaultImg src="/images/couple/Queen.jpeg" />
-      <DefaultImg src="/images/couple/shy.jpeg" />
-      <DefaultImg src="/images/couple/tiara.jpeg" />
-      <DefaultImg src="/images/couple/quokkaalone.jpeg" />
-      <DefaultImg src="/images/couple/one.jpeg" />
-      <DefaultImg src="/images/couple/sunnyalone.jpeg" />
-      <DefaultImg src="/images/couple/sunnyinfront.jpeg" />
-      <DefaultImg src="/images/couple/sunnytwo.jpeg" />
-      <DefaultImg src="/images/couple/yellow-shoes.jpeg" />
-      <DefaultImg src="/images/couple/frame.jpeg" />
-    </ImageDefaultContainer><SpreadButton onClick={() => setImageActive(!imageActive)}>
-        {!imageActive ? '사진 펼쳐서 크게 보기' : '사진 슬라이드로 보기'}
-        <span>{!imageActive ? '🔼' : '🔽'}</span>
-      </SpreadButton><ImageContainer active={imageActive}>
-        <FirstImg src="/images/couple/blue-shy.jpeg" />
-        <FirstImg src="/images/couple/couple-shoes.jpeg" />
-        <FirstImg src="/images/couple/next-line.jpeg" />
-        <FirstImg src="/images/couple/kuho.jpeg" />
-        <FirstImg src="/images/couple/Queen.jpeg" />
-        <FirstImg src="/images/couple/shy.jpeg" />
-        <FirstImg src="/images/couple/tiara.jpeg" />
-        <FirstImg src="/images/couple/quokkaalone.jpeg" />
-        <FirstImg src="/images/couple/one.jpeg" />
-        <FirstImg src="/images/couple/sunnyalone.jpeg" />
-        <FirstImg src="/images/couple/sunnyinfront.jpeg" />
-        <FirstImg src="/images/couple/sunnytwo.jpeg" />
-        <FirstImg src="/images/couple/yellow-shoes.jpeg" />
-        <FirstImg src="/images/couple/frame.jpeg" />
-      </ImageContainer></>
+    <>
+      <><ImageDefaultContainer active={imageActive}>
+        <DefaultImg src="/images/couple/blue-shy.jpeg" onClick={()=>setClickedIndex(1)}/>
+        <DefaultImg src="/images/couple/couple-shoes.jpeg" onClick={()=>setClickedIndex(2)}/>
+        <DefaultImg src="/images/couple/next-line.jpeg" onClick={()=>setClickedIndex(3)}/>
+        <DefaultImg src="/images/couple/kuho.jpeg" onClick={()=>setClickedIndex(4)}/>
+        <DefaultImg src="/images/couple/Queen.jpeg" onClick={()=>setClickedIndex(5)}/>
+        <DefaultImg src="/images/couple/shy.jpeg" onClick={()=>setClickedIndex(6)}/>
+        <DefaultImg src="/images/couple/tiara.jpeg" onClick={()=>setClickedIndex(7)}/>
+        <DefaultImg src="/images/couple/quokkaalone.jpeg" onClick={()=>setClickedIndex(8)}/>
+        <DefaultImg src="/images/couple/one.jpeg" onClick={()=>setClickedIndex(9)}/>
+        <DefaultImg src="/images/couple/sunnyalone.jpeg" onClick={()=>setClickedIndex(10)}/>
+        <DefaultImg src="/images/couple/sunnyinfront.jpeg" onClick={()=>setClickedIndex(11)}/>
+        <DefaultImg src="/images/couple/sunnytwo.jpeg" onClick={()=>setClickedIndex(12)}/>
+        <DefaultImg src="/images/couple/yellow-shoes.jpeg" onClick={()=>setClickedIndex(13)}/>
+        <DefaultImg src="/images/couple/frame.jpeg" onClick={()=>setClickedIndex(14)}/>
+      </ImageDefaultContainer>
+        <SpreadButton onClick={() => setImageActive(!imageActive)}>
+          {!imageActive ? '사진 펼쳐서 크게 보기' : '사진 슬라이드로 보기'}
+          <span>{!imageActive ? '🔼' : '🔽'}</span>
+        </SpreadButton>
+        <ImageContainer active={imageActive}>
+          <FirstImg src="/images/couple/blue-shy.jpeg" />
+          <FirstImg src="/images/couple/couple-shoes.jpeg" />
+          <FirstImg src="/images/couple/next-line.jpeg" />
+          <FirstImg src="/images/couple/kuho.jpeg" />
+          <FirstImg src="/images/couple/Queen.jpeg" />
+          <FirstImg src="/images/couple/shy.jpeg" />
+          <FirstImg src="/images/couple/tiara.jpeg" />
+          <FirstImg src="/images/couple/quokkaalone.jpeg" />
+          <FirstImg src="/images/couple/one.jpeg" />
+          <FirstImg src="/images/couple/sunnyalone.jpeg" />
+          <FirstImg src="/images/couple/sunnyinfront.jpeg" />
+          <FirstImg src="/images/couple/sunnytwo.jpeg" />
+          <FirstImg src="/images/couple/yellow-shoes.jpeg" />
+          <FirstImg src="/images/couple/frame.jpeg" />
+        </ImageContainer></>
+
+      <PopUpImg layerActive={clickedIndex !== 0}>
+        <CloseBtn onClick={()=>setClickedIndex(0)}>❌</CloseBtn>
+          {(clickedIndex === 1) && (<LayerImg src="/images/couple/blue-shy.jpeg" />) }
+          {(clickedIndex === 2) && (<LayerImg src="/images/couple/couple-shoes.jpeg" />)}
+          {(clickedIndex === 3) && (<LayerImg src="/images/couple/next-line.jpeg" />)}
+          {(clickedIndex === 4) && (<LayerImg src="/images/couple/kuho.jpeg" />)}
+          {(clickedIndex === 5) && (<LayerImg src="/images/couple/Queen.jpeg" />)}
+          {(clickedIndex === 6) && (<LayerImg src="/images/couple/shy.jpeg" />)}
+          {(clickedIndex === 7) && (<LayerImg src="/images/couple/tiara.jpeg" />)}
+          {(clickedIndex === 8) && (<LayerImg src="/images/couple/quokkaalone.jpeg" />)}
+          {(clickedIndex === 9) && (<LayerImg src="/images/couple/one.jpeg" />)}
+          {(clickedIndex === 10) && (<LayerImg src="/images/couple/sunnyalone.jpeg" />)}
+          {(clickedIndex === 11) && (<LayerImg src="/images/couple/sunnyinfront.jpeg" />)}
+          {(clickedIndex === 12) && (<LayerImg src="/images/couple/sunnytwo.jpeg" />)}
+          {(clickedIndex === 13) && (<LayerImg src="/images/couple/yellow-shoes.jpeg" />)}
+          {(clickedIndex === 14) && (<LayerImg src="/images/couple/frame.jpeg" />)}
+        {/* <FirstImg src="/images/couple/blue-shy.jpeg" /> */}
+      </PopUpImg></>
   )
 }
 
